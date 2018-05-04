53656187a34f0cdf2505c5c9027b7030053dba59
Victor <victor.ciresica@gmail.com>
2018-05-04 21:08:25 +0300

    Clase

--- src/Conferinta.java
+++ src/Conferinta.java
@@ -1,2 +1,28 @@
+import java.sql.Time;
+import java.util.Date;
+
 public class Conferinta {
+    private Sala sala;
+    private String nume;
+    private int nrParticipanti;
+    private Date data;
+    private Time ora;
+    private Angajat organizator;
+    private ArrayList<Angajat> listaParticipanti;
+
+    public void adaugareParticipant(Angajat angajat) {
+
+    }
+
+    public void modificareSala(Sala sala) {
+
+    }
+
+    public void modificareData(Date data) {
+
+    }
+
+    public void mesajParticipanti(String mesaj) {
+
+    }
 }
